a700a99891ba911d20faf7ae284ef82d1732e6d7
nik kyriakidis <nik@pushworth.com>
2015-10-01 16:27:56 +1000

    completed filter class

--- src/Filters/Filter.php
+++ src/Filters/Filter.php
@@ -1,13 +1,23 @@
 <?php
+
 /*
  * WORDSMITH FILTER CLASS
  * 
  * this class is the base object for a filter to be added into wordpress.
  * 
  */
+
 namespace nk2580\wordsmith\Filters;
 
-class Filter{
-	
-	
+class Filter {
+
+    private $tag;
+    private $callback;
+    private $priority;
+    private $args;
+
+    public function __construct() {
+         add_filter($this->tag, array($this, $this->callback), $this->priority, $this->args);
+    }
+
 }

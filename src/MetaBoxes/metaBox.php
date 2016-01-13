c8975625f47002df8add30b0343eafbcc9f70b07
nik kyriakidis <nik@pushworth.com>
2016-01-13 10:16:56 +1000

    Signed-off-by: nik kyriakidis <nik@pushworth.com>

--- src/MetaBoxes/metaBox.php
+++ src/MetaBoxes/metaBox.php
@@ -89,9 +89,9 @@ class MetaBox {
      */
     public function content($post) {
         foreach ($this->fields as $field) {
-            $value = get_post_meta($post->ID, $field['name'], true);
-            print_r($field['name']);
-            $control = new $field['control']($field['name'], $field['label'], $value);
+            $value = get_post_meta($post->ID, $field['meta-key'], true);
+            print_r($field['meta-key']);
+            $control = new $field['control']($field['meta-key'], $field['label'], $value);
             $control->printField();
         }
     }

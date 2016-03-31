32dddaf364fff89b23bba0b70199da1a02603a52
nik kyriakidis <nik@pushworth.com>
2016-03-31 15:46:00 +1000

    Signed-off-by: nik kyriakidis <nik@pushworth.com>

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -93,7 +93,7 @@ class Route {
 
     private function regexURI() {
         $raw = preg_replace('/\{(.*?)\}/', "([a-zA-Z0-9]+)", $this->uri);
-        $processed = preg_replace("/", "\/", $raw);
+        $processed = preg_replace("/\//", "\/", $raw);
         return "/".$processed."$/";
     }
 
